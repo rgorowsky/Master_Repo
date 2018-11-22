38a295635a8c6fe2558954b540db827e86c4ce4a
Robby Lazarus <rgorowsky@gmail.com>
2018-11-21 23:45:58 -0600

    adding most recent work as a save point

---
+++ Homework13/scrape_mars.py
@@ -0,0 +1,10 @@
+# Dependencies
+from bs4 import BeautifulSoup
+import requests
+import os
+import pymongo
+from splinter import Browser
+from splinter.exceptions import ElementDoesNotExist
+import pandas as pd
+from zipfile import ZipFile
+from IPython.display import display_html
